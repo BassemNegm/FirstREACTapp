3da2b7b9593a264c6555f55c0960fd87ab70ddd0
BassemNegm <bassem.negm91@mail.com>
2023-09-08 15:29:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Navbar from './Navbar';
+import Card from './Card';
+
+function App() {
+  return (
+    <div> 
+      <Navbar />
+      <Card /> <Card /> <Card />
+    </div>
+  );
+}
+
+export default App;
